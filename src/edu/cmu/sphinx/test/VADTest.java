efe978a0087033d0e6e2e836e3eadd9372ad5072
himgod <himgod@sina.com>
2015-05-31 18:08:19 +0800

    update VADTest.file,add a line NonSpeechDataFilter object

--- src/edu/cmu/sphinx/test/VADTest.java
+++ src/edu/cmu/sphinx/test/VADTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.sphinx.test;
 
+import edu.cmu.sphinx.frontend.endpoint.NonSpeechDataFilter;
 import edu.cmu.sphinx.frontend.endpoint.SpeechClassifier;
 import edu.cmu.sphinx.frontend.endpoint.SpeechMarker;
 import edu.cmu.sphinx.util.Microphone;
@@ -15,11 +16,14 @@ public class VADTest {
 		Microphone microphone = new Microphone(16000,16,1,true,true,true,10,true,"average",0,"default",6400);
 		SpeechClassifier classifier = new SpeechClassifier(10,0.003,10,0);
 		SpeechMarker marker = new SpeechMarker(200,500,50,30,50,15.0);
+		NonSpeechDataFilter filter = new NonSpeechDataFilter();
+		
 		marker.initialize();
 		microphone.initialize();
 		//classifier as microphone for data source
 		classifier.setPredecessor(microphone);
 		marker.setPredecessor(classifier);
+		filter.setPredecessor(marker);
 		if(microphone.startRecording())
 		{
 			System.out.println("start recognizing");
@@ -27,7 +31,7 @@ public class VADTest {
 		
 		while(true)
 		{
-			marker.getData();
+			System.out.println(filter.getData());
 		}
 		
 	}	
